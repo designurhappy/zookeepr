7daa7db0a1f727b3dd4053fb70384acc289fe601
Moo Point <beachgal0105@gmail.com>
2020-08-23 18:30:19 -0700

    update heroku

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-const { animals } = require('./data/animals');
+const { animals } = require('./api/animals');
 
 function filterByQuery(query, animalsArray) {
     let filteredResults = animalsArray;
